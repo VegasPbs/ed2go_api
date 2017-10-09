1a05f5516dc6b14750e0774cb794e643b8b336f0
Melody Moselle <mmoselle@vegaspbs.org>
2017-10-09 16:12:07 -0700

    Add 'ServiceConfig.java'

---
+++ src/main/java/Ed2goApiService.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ed2goApiService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ed2goApiService.class, args);
+    }
+}
---
+++ src/main/java/com/vegaspbs/service/ServiceConfig.java
@@ -0,0 +1,31 @@
+package com.vegaspbs.service;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.ws.client.core.WebServiceTemplate;
+
+@Configuration
+public class ServiceConfig {
+
+    private static final String CONTEXT_PATH = "com.vegaspbs.types.ed2go";
+    private static final String DEFAULT_URI = "https://api.ed2go.com/sandbox/api/4.1/";
+
+    @Bean
+    Jaxb2Marshaller jaxb2Marshaller() {
+        Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
+        jaxb2Marshaller.setContextPath(CONTEXT_PATH);
+
+        return jaxb2Marshaller;
+    }
+
+    @Bean
+    public WebServiceTemplate webServiceTemplate() {
+        WebServiceTemplate webServiceTemplate = new WebServiceTemplate();
+        webServiceTemplate.setMarshaller(jaxb2Marshaller());
+        webServiceTemplate.setUnmarshaller(jaxb2Marshaller());
+        webServiceTemplate.setDefaultUri(DEFAULT_URI);
+
+        return webServiceTemplate;
+    }
+}
